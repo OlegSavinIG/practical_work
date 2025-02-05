4ae5c25ec64dd87befbbe033db532634908ae76f
Oleg <oleg211190gm@yandex.ru>
2025-02-05 14:41:33 +0300

    ComplexTask

--- src/main/java/work/concurrency/barrier/ComplexTask.java
+++ src/main/java/work/concurrency/barrier/ComplexTask.java
@@ -1,21 +1,25 @@
 package work.concurrency.barrier;
 
+import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CyclicBarrier;
 
-public class ComplexTask implements Runnable {
+public class ComplexTask implements Callable<Integer> {
     private final CyclicBarrier barrier;
+    private final Random random = new Random();
 
     public ComplexTask(CyclicBarrier barrier) {
         this.barrier = barrier;
     }
 
     @Override
-    public void run() {
+    public Integer call() {
         try {
-            Thread.sleep(500);
+            int result = random.nextInt(10);
             System.out.println(Thread.currentThread().getName() + " waiting");
             barrier.await();
-            System.out.println(Thread.currentThread().getName() + " finished");
+            System.out.println(Thread.currentThread().getName() + " finished with result " + result);
+            return result;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- src/main/java/work/concurrency/barrier/ComplexTaskExecutor.java
+++ src/main/java/work/concurrency/barrier/ComplexTaskExecutor.java
@@ -1,8 +1,13 @@
 package work.concurrency.barrier;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ComplexTaskExecutor {
     private final int threadCount;
@@ -12,19 +17,33 @@ public class ComplexTaskExecutor {
     }
 
     public void executeTasks(int taskCount) {
+        List<Future<Integer>> result = Collections.synchronizedList(new ArrayList<>());
         ExecutorService executor = Executors.newFixedThreadPool(threadCount);
-        CyclicBarrier barrier = new CyclicBarrier(taskCount,
-                () -> {
-                    System.out.println("All tasks finished");
-                    executor.shutdown();
-                });
+        CyclicBarrier barrier = new CyclicBarrier(taskCount, () -> {
+            System.out.println("All tasks crossed barrier");
+        });
 
         try {
             for (int i = 0; i < taskCount; i++) {
-                executor.execute(new ComplexTask(barrier));
+                Future<Integer> submit = executor.submit(new ComplexTask(barrier));
+                result.add(submit);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+       int sum = computeResult(result);
+       System.out.println("Final sum = " + sum + " " + Thread.currentThread().getName());
+    }
+
+    private int computeResult(List<Future<Integer>> result) {
+        return result.stream()
+                .mapToInt(future -> {
+                    try {
+                        return future.get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .sum();
     }
 }
